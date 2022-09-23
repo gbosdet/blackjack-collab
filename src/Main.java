415b4f771952555e544bd2df08299807a4c86a40
Geoff Bosdet <geoffrey.bosdet@aberdeenhall.com>
2022-09-23 13:23:30 -0700

    Fixed card and started main

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     static Player player;
     static Scanner input;
 
-    public static void main(){
+    public static void main(String[] args){
         input = new Scanner(System.in);
         /* TODO: Use other classes to make it work */
         System.out.println("Hello! What is your name?");
-
+        player = new Player(input.nextLine());
 
 
 
